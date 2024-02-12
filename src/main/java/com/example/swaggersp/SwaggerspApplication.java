69639d3d950b8fb72498505a715e8ed35d499251
iamramyak <iamramyak7@gmail.com>
2024-02-12 16:57:53 +0530

    swagger-sb

---
+++ src/main/java/com/example/swaggersp/SwaggerspApplication.java
@@ -0,0 +1,13 @@
+package com.example.swaggersp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerspApplication.class, args);
+	}
+
+}
